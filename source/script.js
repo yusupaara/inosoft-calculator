2492eae324175bcb3c96aedbb80af30880740d20
yusuplusminus <yoesoep17@gmail.com>
2023-04-01 17:16:24 +0700

    Update script.js
    fix bug delete button

--- source/script.js
+++ source/script.js
@@ -9,11 +9,26 @@ buttons.forEach((item) => {
       let string = display.innerText.toString();
       if (display.innerText == "0") {
         //do nothing
-      } else{
-        display.innerText = string.substr(0, string.length - 1);
+      } else {
+        if (display.innerText.length == 1) {
+          display.innerText = "0";
+        } else {
+          display.innerText = string.substr(0, string.length - 1);
+        }
       }
-    } else if (display.innerText == "0" && (item.id == "1"||item.id == "2"||item.id == "3"||item.id == "4"||item.id == "5"||item.id == "6"||item.id == "7"||item.id == "8"||item.id == "9")) {
-        display.innerText = item.id;
+    } else if (
+      display.innerText == "0" &&
+      (item.id == "1" ||
+        item.id == "2" ||
+        item.id == "3" ||
+        item.id == "4" ||
+        item.id == "5" ||
+        item.id == "6" ||
+        item.id == "7" ||
+        item.id == "8" ||
+        item.id == "9")
+    ) {
+      display.innerText = item.id;
     } else if (display.innerText != "" && item.id == "equal") {
       display.innerText = eval(display.innerText);
     } else {
